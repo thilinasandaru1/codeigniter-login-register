ca061bc7dac78862665441862146930660c76d7b
Thilina Sandaruwan <thilinasandaru1@gmail.com>
2018-08-02 16:50:00 +0530

    added controllers models and some views

---
+++ application/controllers/Auth.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	public function login()
+	{
+		$this->load->view('login');
+	}
+}
